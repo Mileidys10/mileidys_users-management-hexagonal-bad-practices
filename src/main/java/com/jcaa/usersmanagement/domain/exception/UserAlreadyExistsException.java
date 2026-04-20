36440cc747af6a8403bf5d5216ce5bcb9527a20d
Mileidys10 <agamezmileidys@gmail.com>
2026-04-20 11:30:55 -0500

    Arreglo de la regla 10, remplazando strings magicas por constantes

--- src/main/java/com/jcaa/usersmanagement/domain/exception/UserAlreadyExistsException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/UserAlreadyExistsException.java
@@ -2,13 +2,14 @@ package com.jcaa.usersmanagement.domain.exception;
 
 public final class UserAlreadyExistsException extends DomainException {
 
-  private UserAlreadyExistsException(final String message) {
-    super(message);
-  }
+    private UserAlreadyExistsException(final String message) {
+        super(message);
+    }
 
-  public static UserAlreadyExistsException becauseEmailAlreadyExists(final String email) {
-    // VIOLACIÓN Regla 10: texto de error hardcodeado directamente en el método fábrica.
-    // Debe usarse una constante con nombre descriptivo en lugar de un String literal.
-    return new UserAlreadyExistsException(String.format("A user with email '%s' already exists.", email));
-  }
+    private static final String EMAIL_ALREADY_EXISTS_MSG = "A user with email '%s' already exists.";
+
+    public static UserAlreadyExistsException becauseEmailAlreadyExists(final String email) {
+
+        return new UserAlreadyExistsException(String.format(EMAIL_ALREADY_EXISTS_MSG, email));
+    }
 }
