1394398c6edc87f375a5bc5d0bfe5090ab511ae8
Mileidys10 <agamezmileidys@gmail.com>
2026-04-20 11:18:57 -0500

    Arreglo de la regla 10, remplazando strings magicas por constantes

--- src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
@@ -2,16 +2,21 @@ package com.jcaa.usersmanagement.domain.exception;
 
 public final class InvalidUserPasswordException extends DomainException {
 
-  private InvalidUserPasswordException(final String message) {
-    super(message);
-  }
+    private InvalidUserPasswordException(final String message) {
+        super(message);
+    }
 
-  public static InvalidUserPasswordException becauseValueIsEmpty() {
-    return new InvalidUserPasswordException("The user password must not be empty.");
-  }
+    private static final String EMPTY_PASSWORD_MSG = "The user password must not be empty.";
+    private static final String SHORT_PASSWORD_MSG = "The user password must have at least %d characters.";
 
-  public static InvalidUserPasswordException becauseLengthIsTooShort(final int minimumLength) {
-    return new InvalidUserPasswordException(
-        String.format("The user password must have at least %d characters.", minimumLength));
-  }
+
+
+    public static InvalidUserPasswordException becauseValueIsEmpty() {
+        return new InvalidUserPasswordException(EMPTY_PASSWORD_MSG);
+    }
+
+    public static InvalidUserPasswordException becauseLengthIsTooShort(final int minimumLength) {
+        return new InvalidUserPasswordException(
+                String.format(SHORT_PASSWORD_MSG, minimumLength));
+    }
 }
