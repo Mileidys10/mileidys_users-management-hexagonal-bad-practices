d9f9178e46110b7d51c4294464b17be468aba4e3
Mileidys10 <agamezmileidys@gmail.com>
2026-04-20 11:37:21 -0500

    Arreglo de la regla 10, remplazando strings magicas por constantes

--- src/main/java/com/jcaa/usersmanagement/domain/exception/UserNotFoundException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/UserNotFoundException.java
@@ -5,10 +5,9 @@ public final class UserNotFoundException extends DomainException {
   private UserNotFoundException(final String message) {
     super(message);
   }
+private static final String NOT_FOUND_ID_MSG = "The user with id '%s' was not found.";
 
   public static UserNotFoundException becauseIdWasNotFound(final String userId) {
-    // VIOLACIÓN Regla 10: texto de error hardcodeado directamente en el método fábrica.
-    // Debe usarse una constante con nombre descriptivo en lugar de un String literal.
-    return new UserNotFoundException(String.format("The user with id '%s' was not found.", userId));
+    return new UserNotFoundException(String.format(NOT_FOUND_ID_MSG, userId));
   }
 }
