35161d7215776e6b0ad0b82d50f7e1bd80e8a887
Mileidys10 <agamezmileidys@gmail.com>
2026-04-21 13:54:07 -0500

    Arreglo de la regla 10, usando constante para el mensaje de error yMétodos de validación privados y expresivos

--- src/main/java/com/jcaa/usersmanagement/domain/valueobject/UserId.java
+++ src/main/java/com/jcaa/usersmanagement/domain/valueobject/UserId.java
@@ -1,29 +1,32 @@
 package com.jcaa.usersmanagement.domain.valueobject;
 
 import com.jcaa.usersmanagement.domain.exception.InvalidUserIdException;
+import java.util.Objects;
+
 
 public record UserId(String value) {
 
-  public UserId {
-    // VIOLACIÓN Regla 4: se usa == null en lugar de Objects.requireNonNull() o Objects.isNull().
-    // Para objetos siempre debe usarse Objects.isNull/nonNull, nunca operadores == o !=.
-    if (value == null) {
-      throw new NullPointerException("UserId cannot be null");
-    }
-    final String normalizedValue = value.trim();
-    validateNotEmpty(normalizedValue);
-    // asigna el valor normalizado al componente
-    value = normalizedValue;
-  }
+    private static final String NULL_ID_MSG = "UserId cannot be null";
 
-  private static void validateNotEmpty(final String normalizedValue) {
-    if (normalizedValue.isEmpty()) {
-      throw InvalidUserIdException.becauseValueIsEmpty();
-    }
-  }
+    public UserId {
+        Objects.requireNonNull(value, NULL_ID_MSG);
 
-  @Override
-  public String toString() {
-    return value;
-  }
+        final String normalizedValue = value.trim();
+
+        validateNotEmpty(normalizedValue);
+
+        value = normalizedValue;
+    }
+
+
+    private void validateNotEmpty(final String id) {
+        if (id.isEmpty()) {
+            throw InvalidUserIdException.becauseValueIsEmpty();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
