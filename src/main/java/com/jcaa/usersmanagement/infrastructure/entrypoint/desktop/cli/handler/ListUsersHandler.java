31dd9237b21fb87f90bcb347d292460e20308ecb
Mileidys10 <agamezmileidys@gmail.com>
2026-04-23 00:06:19 -0500

    arreglo dE REGLA 4,6 Y 10

--- src/main/java/com/jcaa/usersmanagement/infrastructure/entrypoint/desktop/cli/handler/ListUsersHandler.java
+++ src/main/java/com/jcaa/usersmanagement/infrastructure/entrypoint/desktop/cli/handler/ListUsersHandler.java
@@ -3,18 +3,27 @@ package com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.cli.handler;
 import com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.cli.io.UserResponsePrinter;
 import com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.controller.UserController;
 import com.jcaa.usersmanagement.infrastructure.entrypoint.desktop.dto.UserResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 
+import java.util.List;
+import java.util.logging.Level;
+
+@Log
 @RequiredArgsConstructor
 public final class ListUsersHandler implements OperationHandler {
 
-  private final UserController userController;
-  private final UserResponsePrinter printer;
+    private static final String LIST_USERS_LOG = "All users list was requested.";
 
-  @Override
-  public void handle() {
-    final List<UserResponse> users = userController.listAllUsers();
-    printer.printList(users);
-  }
+    private final UserController userController;
+    private final UserResponsePrinter printer;
+
+    @Override
+    public void handle() {
+        log.log(Level.INFO, LIST_USERS_LOG);
+
+        final List<UserResponse> users = userController.listAllUsers();
+
+        printer.printList(users);
+    }
 }
